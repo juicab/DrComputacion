571149b0e75a40a7269f4449246151559b0ecb1c
juicab <32874265+juicab@users.noreply.github.com>
2019-08-11 17:23:12 -0500

    Creacion del Proyecto

---
+++ DrComputacion/Proximamente/Controllers/ReparacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proximamente.Controllers
+{
+    public class ReparacionController : Controller
+    {
+        // GET: Reparacion
+        public ActionResult Proximamente()
+        {
+            return View();
+        }
+    }
+}
---
+++ DrComputacion/Proximamente/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proximamente.MvcApplication" Language="C#" %>
